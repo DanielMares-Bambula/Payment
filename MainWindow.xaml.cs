bf24c5cd4f98d0cc9b8fcaca07211d5fee7cc695
DanielMares-Bambula <79077162+DanielMares-Bambula@users.noreply.github.com>
2021-03-28 11:15:22 +0200

    Přidejte soubory projektu.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Payment
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    class Person
+    {
+        string name;
+        string surname;
+        string date;
+        public Person(string nameC, string surnameC, string dateC) 
+        {
+            nameC = name;
+            surnameC = surname;
+            dateC = date;
+        }
+        //string Name { get; set; }
+        //string Surname { get; set;}
+        ////var dat = new DateTime();
+    }
+    class Employe : Person
+    {
+        int payment;
+
+    }
+}
